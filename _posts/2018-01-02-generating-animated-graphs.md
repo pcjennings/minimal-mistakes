dd9e6d571b680c95d35a2c98709860df3ff4150f
Paul C. Jennings <jennings.p.c@gmail.com>
2018-01-02 13:20:52 -0800

    start animated graph post

---
+++ _posts/2018-01-02-generating-animated-graphs.md
@@ -0,0 +1,11 @@
+There are a number of ways to generate animated graphs. In this post, I will use a combination of matplotlib and [ImageMagick](http://www.imagemagick.org/script/index.php) to produce a relatively easy and robust way to generate an animated gif of a graph. It is possible to do this directly with matplotlib, however, I found this to be a little fiddly.
+
+## General Method
+
+The easiest way to generate a gif without needing to change pre-existing code too much is to create a large number of individual graphs and then stitch them together. The graphs are created with matplotlib and saved as png files, though any plotting library could be used. ImageMagick is then called to stitch all the individual images together.
+
+## Automating
+
+The automated script is relatively simple.
+
+![animated graph](scripts/animated.gif)
---
+++ scripts/animated_graph.py
@@ -0,0 +1,39 @@
+"""Animate results from a GA search for chemical ordering optimization in
+nanoparticles. Assumes http://www.imagemagick.org/ has been installed."""
+import numpy as np
+import subprocess
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+matplotlib.rc('font', serif='Helvetica Neue Light')
+matplotlib.rcParams.update({'font.size': 22})
+
+p1 = np.random.random_sample(size=20)
+p2 = np.random.random_sample(size=20)
+index = list(range(len(p1)))
+
+step_p1, step_p2, step_index = [], [], []
+fname = []
+for i in index:
+    step_p1.append(p1[i])
+    step_p2.append(p2[i])
+    step_index.append(i)
+
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(111)
+    ax.axis([min(p1)-0.1, max(p1)+0.1, min(p2)-0.1, max(p2)+0.1])
+    ax.scatter(step_p1, step_p2, c=step_index, cmap=cm.rainbow, alpha=0.5)
+    plt.xlabel('variable 1')
+    plt.ylabel('variable 2')
+
+    n = 'graph_{0}.png'.format(i)
+    fname.append(n)
+    fig.savefig(n)
+
+call = 'convert -delay 15 -loop 0 '
+for i in fname:
+    call += i + ' '
+call += 'animated.gif'
+
+subprocess.call(call, shell=True)
